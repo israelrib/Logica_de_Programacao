4f2f463bfcc22d9c228a5db548aa3555258d5c31
israelrib <israel.contato02@gmail.com>
2026-09-02 17:53:17 -0300

    aprendendo sobre variáveis

---
+++ Variaveis/src/main/java/com/mycompany/variaveis/Variaveis.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.variaveis;
+
+/* Comentário de várias linhas */
+// Comentário de uma linha
+
+public class Variaveis {
+    // a classe main é quem executa os codigos
+    public static void main(String[] args) {
+    /* códigos de saída -> são utilizados para imprimir/mostrar dados e mensagens*/    
+        System.out.println("Ola"); // "println" pula uma linha
+        System.out.println("eu sou um programador");
+        
+        System.out.print("Eu ja sei programar"); // "print" não pula linha
+        System.out.print("eu amo java");
+        
+        // CRIAÇÃO DE VARIÁVEIS
+        int idade = 24; // valor inteiro
+        double altura = 1.87; //valor decimal grande
+        String nome = "Israel Santos"; //textos
+        float peso ; //valor decimal pequeno
+        boolean solteiro = true; // valor lógico
+        
+        System.out.println("Olá, " +nome);
+        
+        
+        
+    } // fim da main
+}
+// nenhum código pode ficar aqui fora
+
